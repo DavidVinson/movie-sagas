65866b3dae4c769483e1cfaf67b392013f02c9e2
David Vinson <davidvinson018@gmail.com>
2021-05-29 21:25:53 -0500

    fixed key issue with genre detail

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,17 +1,19 @@
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import {useHistory} from 'react-router-dom';
 
 
 function MovieDetail() {
 
-    //need button for page navigation
-    //endpoint for details page: '/api/movie/detail/:id'
-    //make fetch call to saga 
-    //select info from store
-    //display on page
+    //[x]need button for page navigation
+    //[x]endpoint for details page: '/api/movie/detail/:id'
+    //[x]make fetch call to saga 
+    //[x]select info from store
+    //[x]display on page
 
     const dispatch = useDispatch(); //saga will pick up
+    const history = useHistory();
 
     const { movieId } = useParams(); //movieId from MovieList
     console.log('The selected movie ID', movieId);
@@ -43,9 +45,10 @@ function MovieDetail() {
                     <h1>{movie.title}</h1>
                     <img src={movie.poster} />
                     <div>
-                        {genreDetail.map((genre) => <p>{genre.name}</p>)}
+                        {genreDetail.map((genre) => <p key={genre.id}>{genre.name}</p>)}
                     </div>
                     <p>Description: {movie.description}</p>
+                    <button onClick={() => history.push('/')}>Back to Movie List</button>
                 </div>
             )}
         </main>
